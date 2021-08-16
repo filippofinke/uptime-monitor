a1e4f7b0f1c094d3abf658edeb3383b83706823a
filippofinke <filippofinke@protonmail.ch>
2021-08-16 13:16:40 +0200

    :wrench: Add configuration sample

---
+++ services.sample.js
@@ -0,0 +1,13 @@
+let services = [
+  {
+    name: "Localhost",
+    url: "http://localhost",
+  },
+  {
+    name: "Api",
+    url: "http://localhost/api",
+    expectedCode: 401,
+  },
+];
+
+module.exports = services;
