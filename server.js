a340eef59449e91f321a29e81c0313d348130796
filippofinke <filippofinke@protonmail.ch>
2021-09-08 12:27:16 +0000

    :recycle: Add morgan

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 const sqlite3 = require("sqlite3").verbose();
 
 let plugins = [];
 require("fs")
-  .readdirSync("./plugins")
-  .forEach((file) => {
-    if (file.endsWith(".js")) {
-      console.log(`Loaded ${file} plugin!`);
-      plugins.push(require("./plugins/" + file));
-    }
-  });
+	.readdirSync("./plugins")
+	.forEach((file) => {
+		if (file.endsWith(".js")) {
+			console.log(`Loaded ${file} plugin!`);
+			plugins.push(require("./plugins/" + file));
+		}
+	});
 
 const db = new sqlite3.Database("./database/history.db", () => {
-  db.run(
-    `CREATE TABLE IF NOT EXISTS history (
+	db.run(
+		`CREATE TABLE IF NOT EXISTS history (
     service VARCHAR(255),
     timestamp TIMESTAMP,
     status VARCHAR(10),
@@ -24,10 +25,10 @@ const db = new sqlite3.Database("./database/history.db", () => {
     ms INT,
     PRIMARY KEY(service, timestamp)
     );`,
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
+		(err) => {
+			if (err) console.log(err);
+		}
+	);
 });
 
 const app = express();
@@ -38,104 +39,105 @@ process.env.INTERVAL = process.env.INTERVAL || 5000;
 let lastUpdate = null;
 let services;
 try {
-  services = require("./services.js");
+	services = require("./services.js");
 } catch (error) {
-  console.log(
-    "services.js not found! Please rename the file services.sample.js to services.js!"
-  );
-  process.exit(0);
+	console.log(
+		"services.js not found! Please rename the file services.sample.js to services.js!"
+	);
+	process.exit(0);
 }
 
 const checkService = async (service) => {
-  let ms = null;
-  let code = null;
-  let status = "offline";
-  let text = "Connection error";
+	let ms = null;
+	let code = null;
+	let status = "offline";
+	let text = "Connection error";
 
-  try {
-    let start = Date.now();
-    let response = await fetch(service.url, {
-      follow: 0,
-    });
-    let end = Date.now();
-    ms = end - start;
-    code = response.status;
-    text = response.statusText || "";
-    if (code === (service.expectedCode || 200)) {
-      status = ms < 250 ? "online" : "degraded";
-    } else {
-      status = "offline";
-    }
-  } catch (err) {}
+	try {
+		let start = Date.now();
+		let response = await fetch(service.url, {
+			follow: 0,
+		});
+		let end = Date.now();
+		ms = end - start;
+		code = response.status;
+		text = response.statusText || "";
+		if (code === (service.expectedCode || 200)) {
+			status = ms < 250 ? "online" : "degraded";
+		} else {
+			status = "offline";
+		}
+	} catch (err) {}
 
-  service.lastStatus = service.status;
-  service.status = status;
-  service.statusCode = code;
-  service.statusText = text;
-  service.ms = ms;
-  lastUpdate = Date.now();
+	service.lastStatus = service.status;
+	service.status = status;
+	service.statusCode = code;
+	service.statusText = text;
+	service.ms = ms;
+	lastUpdate = Date.now();
 
-  if (service.lastStatus && service.lastStatus != service.status) {
-    for (let plugin of plugins) {
-      plugin(service);
-    }
-  }
+	if (service.lastStatus && service.lastStatus != service.status) {
+		for (let plugin of plugins) {
+			plugin(service);
+		}
+	}
 
-  db.run(
-    "INSERT INTO history VALUES (?, ?, ?, ?, ?)",
-    [service.name, Date.now(), status, text, ms],
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
+	db.run(
+		"INSERT INTO history VALUES (?, ?, ?, ?, ?)",
+		[service.name, Date.now(), status, text, ms],
+		(err) => {
+			if (err) console.log(err);
+		}
+	);
 };
 
 const checkStatus = () => {
-  for (let service of services) {
-    checkService(service);
-  }
+	for (let service of services) {
+		checkService(service);
+	}
 };
 
 checkStatus();
 setInterval(checkStatus, process.env.INTERVAL);
 
+app.use(morgan("common"));
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 
 app.get("/services", (req, res) => {
-  return res.json({
-    lastUpdate,
-    services: services.map((service) => {
-      return {
-        name: service.name,
-        status: service.status,
-        statusCode: service.statusCode,
-        statusText: service.statusText,
-        ms: service.ms,
-      };
-    }),
-  });
+	return res.json({
+		lastUpdate,
+		services: services.map((service) => {
+			return {
+				name: service.name,
+				status: service.status,
+				statusCode: service.statusCode,
+				statusText: service.statusText,
+				ms: service.ms,
+			};
+		}),
+	});
 });
 
 app.get("/service/:name", (req, res) => {
-  let service = req.params.name;
-  let count = req.query.count;
-  if (!count || count > 100) count = 100;
+	let service = req.params.name;
+	let count = req.query.count;
+	if (!count || count > 100) count = 100;
 
-  if (services.find((s) => s.name === service)) {
-    db.all(
-      "SELECT timestamp, status, statusText, ms FROM history WHERE service = ? ORDER BY timestamp DESC LIMIT ?",
-      [service, Number(count)],
-      (err, rows) => {
-        if (err) console.log(err);
-        return res.json(rows.reverse());
-      }
-    );
-  } else {
-    return res.sendStatus(404);
-  }
+	if (services.find((s) => s.name === service)) {
+		db.all(
+			"SELECT timestamp, status, statusText, ms FROM history WHERE service = ? ORDER BY timestamp DESC LIMIT ?",
+			[service, Number(count)],
+			(err, rows) => {
+				if (err) console.log(err);
+				return res.json(rows.reverse());
+			}
+		);
+	} else {
+		return res.sendStatus(404);
+	}
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`uptime-monitor server listening on port ${process.env.PORT}`);
+	console.log(`uptime-monitor server listening on port ${process.env.PORT}`);
 });
